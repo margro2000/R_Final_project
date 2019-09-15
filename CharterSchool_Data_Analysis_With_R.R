1581d9fa1b7485cce72829687edd1efde62c2cd7
Margarita Groisman <mg@lawn-128-61-44-228.lawn.gatech.edu>
2019-09-15 15:10:27 -0400

    first commit

--- CharterSchool_Data_Analysis_With_R.R
+++ CharterSchool_Data_Analysis_With_R.R
@@ -1,3 +1,7 @@
+#Analysis of Charter School Data for Open R Minimester Course
+#Author: Margarita Groisman (margarita.groisman@gatech.edu)
+#2019-09-14
+
 install.packages("tidyverse")
 install.packages('DataExplorer')
 library(DataExplorer)
@@ -23,11 +27,32 @@ Girls_Prepatory_Data <- filter(df, V1=="84M330")
 selected_Girls_Prep_Data <- select(Girls_Prepatory_Data, "V1", "V3", "V6":"V17")
 
 #now this specific school with specific set of data has been filteres and selected and is ready for analysis
-#My data analysis:
+
+#My explatory data analysis:
 
 summary(selected_Girls_Prep_Data)
 plot_str(selected_Girls_Prep_Data)
+
 plot_missing(selected_Girls_Prep_Data) #shows that there is no missing data
 plot_histogram(selected_Girls_Prep_Data)
 plot_bar(selected_Girls_Prep_Data)
- 
+
+#My decision tree data model
+
+summary(selected_Girls_Prep_Data)
+
+selected_Girls_Prep_Data <- selected_Girls_Prep_Data %>% filter(!is.na(V1))
+
+
+set.seed(4034)
+
+grade <- createDataPartition(y=selected_Girls_Prep_Data$V1, p=0.8, list=F)
+training <- selected_Girls_Prep_Data[grade,] 
+testing <- selected_Girls_Prep_Data[grade,]
+
+decisionTree <- rpart(V1 ~ . , data = training, cp=0.12)
+
+#Visualize my model
+View(decisionTree)
+fancyRpartPlot(decisionTree)
+
